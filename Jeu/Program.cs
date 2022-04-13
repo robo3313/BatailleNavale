65174c717eaa704a74ffa7fc2a31c4442440e7f8
Tarik012 <tc78200@gmail.com>
2022-04-13 12:28:05 +0200

    ajout menu statut joueur

--- Jeu/Program.cs
+++ Jeu/Program.cs
@@ -6,6 +6,31 @@ using static System.Console;
 namespace  Jeu
 {
     /// <summary>
+    /// classe statique pour les constantes
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// constante pour le logo du jeu
+        /// </summary>
+        public const string _LOGO = @"
+
+
+ ▄▄▄▄    ▄▄▄     ▄▄▄█████▓ ▄▄▄       ██▓ ██▓     ██▓    ▓█████     ███▄    █  ▄▄▄    ██▒   █▓ ▄▄▄       ██▓    ▓█████       ▄▄▄█████▓ ██░ ██ ▓█████      ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ▐██▌ 
+▓█████▄ ▒████▄   ▓  ██▒ ▓▒▒████▄    ▓██▒▓██▒    ▓██▒    ▓█   ▀     ██ ▀█   █ ▒████▄ ▓██░   █▒▒████▄    ▓██▒    ▓█   ▀       ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀     ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀     ▐██▌ 
+▒██▒ ▄██▒██  ▀█▄ ▒ ▓██░ ▒░▒██  ▀█▄  ▒██▒▒██░    ▒██░    ▒███      ▓██  ▀█ ██▒▒██  ▀█▄▓██  █▒░▒██  ▀█▄  ▒██░    ▒███         ▒ ▓██░ ▒░▒██▀▀██░▒███      ▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███       ▐██▌ 
+▒██░█▀  ░██▄▄▄▄██░ ▓██▓ ░ ░██▄▄▄▄██ ░██░▒██░    ▒██░    ▒▓█  ▄    ▓██▒  ▐▌██▒░██▄▄▄▄██▒██ █░░░██▄▄▄▄██ ▒██░    ▒▓█  ▄       ░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄    ░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄     ▓██▒ 
+░▓█  ▀█▓ ▓█   ▓██▒ ▒██▒ ░  ▓█   ▓██▒░██░░██████▒░██████▒░▒████▒   ▒██░   ▓██░ ▓█   ▓██▒▒▀█░   ▓█   ▓██▒░██████▒░▒████▒        ▒██▒ ░ ░▓█▒░██▓░▒████▒   ░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒    ▒▄▄  
+░▒▓███▀▒ ▒▒   ▓▒█░ ▒ ░░    ▒▒   ▓▒█░░▓  ░ ▒░▓  ░░ ▒░▓  ░░░ ▒░ ░   ░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ▐░   ▒▒   ▓▒█░░ ▒░▓  ░░░ ▒░ ░        ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░    ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░    ░▀▀▒ 
+▒░▒   ░   ▒   ▒▒ ░   ░      ▒   ▒▒ ░ ▒ ░░ ░ ▒  ░░ ░ ▒  ░ ░ ░  ░   ░ ░░   ░ ▒░  ▒   ▒▒ ░░ ░░    ▒   ▒▒ ░░ ░ ▒  ░ ░ ░  ░          ░     ▒ ░▒░ ░ ░ ░  ░     ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░    ░  ░ 
+ ░    ░   ░   ▒    ░        ░   ▒    ▒ ░  ░ ░     ░ ░      ░         ░   ░ ░   ░   ▒     ░░    ░   ▒     ░ ░      ░           ░       ░  ░░ ░   ░      ░ ░   ░   ░   ▒   ░      ░      ░          ░ 
+ ░            ░  ░              ░  ░ ░      ░  ░    ░  ░   ░  ░            ░       ░  ░   ░        ░  ░    ░  ░   ░  ░                ░  ░  ░   ░  ░         ░       ░  ░       ░      ░  ░    ░    
+      ░                                                                                  ░                                                                                                          
+";
+        public const string _STRHOST = "en choisissant cette option tu décides d'héberger la partie en attendant qu'un autre joueur te rejoigne";
+        public const string _STRPGUEST = "en choisissant cette option tu décides de rejoindre une partie hébergée par un autre joueur";
+    }
+    /// <summary>
     /// classe correpondant au point d'entrée de l'application
     /// </summary>
     public class Program
--- Jeu/Ship/Boat.cs
+++ Jeu/Ship/Boat.cs
@@ -1,17 +1,26 @@
 ﻿namespace Jeu
 {
-    class Boat
+    /// <summary>
+    /// classe Boat
+    /// </summary>
+    public class Boat
     {
-        String Name;
-        String Type;
-        Dictionary<String, bool> Positions;
-        bool Alive;
+        public string Name;
+        public string Type;
+        public Dictionary<string, bool> Positions;
+        public bool Alive = false;
 
+        /// <summary>
+        /// constructeur de la classe Boat
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <param name="positions"></param>
         public Boat(string name, string type, string[] positions)
         {
             Name = name;
             Type = type;
-            Positions = new Dictionary<String, bool>();
+            Positions = new Dictionary<string, bool>();
             foreach (string position in positions)
             {
                 Positions.Add(position, false);
