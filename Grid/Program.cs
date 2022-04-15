51e304e11f308c2167a4d03af95c980a522e7aad
Oummmmm <doumbia.oumou@gmail.com>
2022-04-15 12:13:31 +0200

    ModifGrid

--- Grid/Program.cs
+++ Grid/Program.cs
@@ -4,8 +4,16 @@ Grid g = new();
 
 g.Display();
 
-Position impact1 = new('E', 3); 
+Position impact1 = new('G', 8); 
 g.AddImpact(impact1);
 
 g.Display();
 
+/*engine
+
+Fleet fleet1 = new Fleet();
+fleet1.AddBoat("Willy", "Orque", new string[] { "A1", "A2", "A3" });
+
+
+Boat b = new ("un", "ulysse", new string[] { "A1", "A2", "A3"});
+g.AddBoat(b);*/
